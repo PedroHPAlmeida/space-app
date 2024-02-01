4ec66d6948cd3cc69413f947b84a7f36f03df85e
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2024-02-01 19:30:46 -0300

    Alinhando Banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import EstilosGlobais from "./componentes/EstilosGlobais";
 import Cabecalho from "./componentes/Cabecalho";
 import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/TituloEstilizado";
+import Galeria from "./componentes/Galeria";
+import bannerBackground from "./assets/banner.png"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,16 +12,40 @@ const FundoGradiente = styled.div`
   min-height: 100vh ;
 `
 
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ConteudoDaGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
 function App() {
   return (
     <FundoGradiente>
       <EstilosGlobais />
-      <Cabecalho />
-      <BarraLateral />
-      <Banner
-        texto="A galeria mais completa de fotos do espaço!A galeria mais completa de fotos do espaço!"
-        backgroundImage="/imagens/galeria/foto-1.png"
-      />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoDaGaleria>
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+            <Galeria />
+          </ConteudoDaGaleria>
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
   )
 }
